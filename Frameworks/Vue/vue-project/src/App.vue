b1e1ed10d82ca6cd21d6cef62c361b2ced751f15
RDCode <rkostalin@gmail.com>
2022-10-07 16:52:44 -0500

    Other commit

--- Frameworks/Vue/vue-project/src/App.vue
+++ Frameworks/Vue/vue-project/src/App.vue
@@ -3,8 +3,34 @@
 <script setup>
   const name = 'Vue3';
   const styleColor = "color: blue";
-  const arrayColors = ['blue', 'red', 'green']
+  const arrayColors = ['blue', 'red', 'green'];
+  const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
   const logged = false;
+
+  const arrayObjFrutas = [
+        {
+            name: "Manzana",
+            price: "$1.00",
+            description: "Una manzana",
+        },
+        {
+            name: "Pera",
+            price: "$2.00",
+            description: "Una pera",
+        },
+        {
+            name: "Naranja",
+            price: "$3.00",
+            description: "Una naranja",
+        },
+    ];
+
+    const fruta = {
+        name: "Naranja",
+        price: "$3.00",
+        description: "Una naranja",
+    };
+
 </script>
 
 <template>
@@ -20,6 +46,26 @@
   <p v-else>Unknow</p>
 
   <h2 v-show="logged">Activo VS v-show</h2>
+
+  <ul>
+    <li v-for="(element, index) of arrayFrutas" :key="index">
+      {{ index }} - {{ element }}
+    </li>
+  </ul>
+
+  <ul>
+    <li v-for="element of arrayObjFrutas" :key="element.name">
+      {{element.name}} 
+      {{element.price}}
+      {{element.description}}
+    </li>
+  </ul>
+
+  <ul>
+    <li v-for="(value, prop, index) in fruta" :key="index">
+      {{index}} - {{prop}}: {{value}}
+    </li>
+  </ul>
 </template>
 
 <style>
