13548cd3917660fe0bad84691fdd30aa4180f944
DevCode <dev.rd.code@gmail.com>
2022-10-22 16:10:14 -0500

    O.C

--- Java/Sockets-Java/sockets/client/Client.java
+++ Java/Sockets-Java/sockets/client/Client.java
@@ -1,29 +1,39 @@
 package sockets.client;
 
-import java.io.*;
-import java.net.*;
+import java.io.*; // Paquete para trabajar con I/O del S.O
+import java.net.*; // Paquete para comunicacion de red
+
+import javax.xml.crypto.Data;
 
 public class Client{
 
 // https://unipython.com/sockets-en-java-cliente-y-servidor/ 
 
-    private static String HOST = "127.0.0.1";
-    private static int PORT = 6060;
+    private static String HOST = "127.0.0.1"; // Host
+    private static int PORT = 6060; // Puerto
     public static void main(String[] args) {
-        Socket socket;
-        DataOutputStream msj;
+        Socket socket;  // Declaracion de socket
+        PrintWriter output;   // Declaracion de mensaje de salida
+        BufferedReader input;
+        String msjServer;
 
         try {
-            socket = new Socket(HOST, PORT);
+            socket = new Socket(HOST, PORT);    // Inicalizacion de socket al servidor
 
-            msj = new DataOutputStream(socket.getOutputStream());
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);   // Inicializacion para salida de msj
 
-            msj.writeUTF("Hola desde el cliente");
+            output.flush();
+            output.println("Hola desde el cliente");  // Escritura / Envio de mensaje al servidor
+            
+            msjServer = input.readLine();
 
-            socket.close();
-        } catch (UnknownHostException e) {
+            System.out.println("Servidor --> " + msjServer);
+
+            socket.close(); // Cierre de conexion con servidor
+        } catch (UnknownHostException e) {  // Error de host desconocido
             System.out.println(e.getMessage());
-        } catch (Exception e) {
+        } catch (Exception e) { // Error general
             System.out.println(e.getMessage());
         }
     }
--- Java/Sockets-Java/sockets/server/Server.java
+++ Java/Sockets-Java/sockets/server/Server.java
@@ -1,47 +1,44 @@
 package sockets.server;
 
-import java.io.*;
-import java.net.*;
+import java.io.*;   // Paquete para manejo de I/O
+import java.net.*;  // Paquete para manejo de comunicaicon en red
 
 public class Server{
     
-    private static int PORT = 6060;
+    private static int PORT = 6060; // Puerto de servidor
 
     public static void main(String[] args) {
-        BufferedReader input;
-        DataOutputStream output;
-        Socket socket;
-        ServerSocket serverSocket;
+        BufferedReader input;   // Declaracion para lectura de bits recibidos
+        PrintWriter output;    // Declaracion para envio o salida de informacion
+        Socket socket;  // Declaracion de socket servidor local
+        ServerSocket serverSocket;  // Declaracion para socket servidor "red"
+        String msjClient;
 
         try {
-            serverSocket = new ServerSocket(PORT);
+            serverSocket = new ServerSocket(PORT);  // Inicializacion de servidor con puerto
 
             System.out.println("Esperando una conexion...");
 
-            socket = serverSocket.accept();
+            socket = serverSocket.accept(); // Aceptacion de conexion de cliente y asignacion de comunicacion
 
             System.out.println("Cliente conectado");
 
             // Entrada y salida de datos
 
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new DataOutputStream(socket.getOutputStream());
-
-            System.out.println("Conexion con el cliente exitosa");
-
-            output.writeUTF("Conexion exitosa");
-
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Incializacion para lectura o recibo de informacion del cliente
+            output = new PrintWriter(socket.getOutputStream(), true);    // Inicializacion para envio o escritura de informacion hacia el cliente
             // Recibo de mensaje
+            
+            while((msjClient = input.readLine()) != null){
+                if(msjClient != null){
+                    System.out.println("Cliente <-- " + msjClient);
+                    break;
+                }
+            };
 
-            String msj = input.readLine();
-
-            System.out.println(msj);
-
-            output.writeUTF("Mensaje recibido");
-
-            output.writeUTF("Cerrando conexion");
-
-            serverSocket.close();
+            output.flush();
+            output.println("Hola desde el servidor");    // Escritura y envio del servidor
+            serverSocket.close();   // Cierre de comunicacion cliente-servidor
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
