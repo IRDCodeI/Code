c0f5eb08f3eb328cd0fc570095c4e0527796a451
RDCode <rkostalin@gmail.com>
2022-10-30 13:43:36 -0500

    O.C

--- Python/sockets-py/sockets-threads/clientB.py
+++ Python/sockets-py/sockets-threads/clientB.py
@@ -3,7 +3,9 @@ import socket
 HOST = '127.0.0.1'
 PORT = 6060
 
+var = input("Mensaje B: ")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
 
-    s.sendall(b'Hola desde el cliente B')
+    s.sendall(str.encode("Cliente B: "+var))
