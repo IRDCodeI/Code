7b10c5ac63c5d7d60dbc89108646b1c50d2bdc1c
DevCode <dev.rd.code@gmail.com>
2022-11-16 17:21:08 -0500

    O.C

--- Python/Data-Science/MRL.py
+++ Python/Data-Science/MRL.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly.graph_objs as go
 
-df = px.data.tips()
-fig = px.scatter(
-    df, x='total_bill', y='tip', opacity=0.65,
-    trendline='ols', trendline_color_override='darkblue'
-)
-fig.show()
+from plotly.offline import plot
+
+x = np.array([2, 6, 8, 8, 12, 16, 20, 20, 22, 26])
+y = np.array([58, 105, 88, 118, 117, 137, 157, 169, 149, 202])
+
+trace = go.Scatter(x = x, y = y, mode = 'markers')
+data = [trace]
+
+plot(data)
