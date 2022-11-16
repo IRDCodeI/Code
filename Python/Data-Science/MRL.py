826ef8cc7193e5ce00a75bf0dd6ccddc7409d4b7
DevCode <dev.rd.code@gmail.com>
2022-11-16 17:01:26 -0500

    O.C

---
+++ Python/Data-Science/MRL.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+df = px.data.tips()
+fig = px.scatter(
+    df, x='total_bill', y='tip', opacity=0.65,
+    trendline='ols', trendline_color_override='darkblue'
+)
+fig.show()
